83f486be552f558fa325be7a59f0460a39412dce
zc <499147867@qq.com>
2020-09-21 16:03:31 +0800

    测试

--- src/main/java/com/njmetro/evaluation/controller/CompanyController.java
+++ src/main/java/com/njmetro/evaluation/controller/CompanyController.java
@@ -1,13 +1,20 @@
 package com.njmetro.evaluation.controller;
 
 
+import com.njmetro.evaluation.domain.Company;
+import com.njmetro.evaluation.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author zc
@@ -15,7 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/company")
+@RequiredArgsConstructor
+@Slf4j
 public class CompanyController {
+    private final CompanyService companyService;
 
+    @GetMapping("/getCompanyList")
+    public List<Company> getCompanyList() {
+        log.info("companyList:{}"+companyService.list());
+        return  companyService.list();
+    }
 }
 
